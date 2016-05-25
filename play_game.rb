0bf501ff7001680ffc63b5083bcd981cea34192e
Jason Jordan <jasonjordan@Jasons-MacBook.local>
2016-05-25 16:40:22 -0400

    Working one round of game

--- play_game.rb
+++ play_game.rb
@@ -8,4 +8,17 @@ require './dealer'
 puts "Welcome to Blackjack!"
 puts
 puts "How much money do you have to wager?"
-wallet = gits.chomp.to_i
+wallet = gets.chomp.to_i
+player = Player.new(wallet)
+
+dealer = Dealer.new
+
+dealer.deal_hand_to(player)
+dealer.deal_hand_to(dealer)
+
+puts "Dealer"
+puts dealer.hand.showing
+puts
+puts
+puts "Your cards"
+puts player.hand.to_s
